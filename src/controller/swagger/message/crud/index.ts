bcb0a3c378c17847b01cec9743fbe5a2235aae7c
matheomiquel <matmatmi@gmail.com>
2023-01-09 18:28:57 +0100

    message crud documentation and unit test done

---
+++ src/controller/swagger/message/crud/index.ts
@@ -0,0 +1,4 @@
+export { readAllMessageSwager } from './readAll'
+export { createMessageSwagger } from './create'
+export { updateMessageSwagger } from './update'
+export { deleteMessageSwagger } from './delete'
